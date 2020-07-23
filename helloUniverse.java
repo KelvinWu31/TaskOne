5c180761ba640a5dfa1fbfecc07d0fc8db29ed4d
KelvinWu31 <60585581+KelvinWu31@users.noreply.github.com>
2020-07-23 19:06:53 -0400

    Second commit, added a for loop to print the statement 10 times

--- helloUniverse.java
+++ helloUniverse.java
@@ -2,7 +2,12 @@
 public class helloUniverse {
 
 	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++ ) { // added a for loop to tell the universe 
+										// i am born 10 times.
 		System.out.println("Hello Universe! I am born.");
+		} 
+		
 	}
 
 }
